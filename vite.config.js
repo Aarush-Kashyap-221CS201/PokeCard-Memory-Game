23b55a1be3c1657881c293384e118b0b4d8a21d1
Aarush Kashyap <aarushk1511@gmail.com>
2024-12-22 13:57:17 +0530

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PokeCard-Memory-Game/', // Replace <repo-name> with your GitHub repository name
   plugins: [react()],
 })
